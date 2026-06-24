9823fd00ed53fc9f30a4f0d715219555e3604520
PierreS64 <phongnt06@gmail.com>
2026-06-24 13:01:33 +0700

    Configure CORS and concurrently for single command startup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Allow any origin for development
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
